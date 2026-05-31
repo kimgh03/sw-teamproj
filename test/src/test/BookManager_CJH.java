8b09ede4af1d35cfb579b6e9862cdc8c2ace3587
goodtu02 <qwe13117@gmail.com>
2026-05-31 19:42:49 +0900

    feat: addBook

--- test/src/test/BookManager_CJH.java
+++ test/src/test/BookManager_CJH.java
@@ -1,5 +1,13 @@
 package test;
 
 public class BookManager_CJH {
+	private Set<String> Books = new HashSet<>();
+
+    public void addBook(String name) {
+        if (books.contains(name)) {
+            throw new IllegalArgumentException("이미 존재하는 학생입니다: " + name);
+        }
+        books.add(name);
+    }
 
 }
